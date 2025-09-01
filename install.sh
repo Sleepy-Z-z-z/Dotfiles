dc9694c36b7549f8d5507d79b6b817f5dfe32953
Sleepy <sasacudragon@gmail.com>
2025-09-01 02:47:19 +0200

    New and improved install script
    added updated list of programs also organized them better and uses less useles commands ie uses pacman for multiple programs  instead of just one,also added systemctl service enables (for pipewire and prelaod) and spicetify should be installed fully now 
    Hopefully error where ~/.config not found is fixed since the script now creats one (Warning some things like the wallpapers still have to be extracted manually )

--- install.sh
+++ install.sh
@@ -1,37 +1,50 @@
 #!/bin/bash
-cd
+
 sudo pacman -Syu
-sudo pacman -Sy timeshift linux linux-headers
-sudo pacman -Sy pavucontrol pipewire wireplumber
-sudo pacman -S git --needed baase-devel
+sudo pacman -Sy hyprland sddm pipewire wireplumber pipewire-pulse
+sudo pacman -S git python-pywal swww base-devel rofi  kitty 7zip yazi nvim bat zsh zoxide fzf fd ripgrep exa thunar fastfetch tmux pavucontrol tty-clock btop ttf-jetbrains-mono-nerd
+sudo pacman -S vivaldi
+sudo pacman -S qutebrowser
+sudo pacman -S snapd flatpak
+sudo pacman -S --needed base-devel
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 cd
-sudo pacman -S hyprland kitty nvim vivaldi
-sudo pacman -S ripgrep zsh fzf yazi zoxide bat fd exa
-paru -Sy preload spotify spicetify
-sudo pacman -S mpv mpris
-sudo pacman -S thunar
-sudo pacman -S sddm
+paru -Syu
+paru -S spotify spicetify
+sudo chmod a+wr /opt/spotify
+sudo chmod a+wr /opt/spotify/Apps -R
+spicetify backup apply
+paru -S preload waypaper cmatrix cava ani-cli
+sudo systemctl --user enable pipewire
+sudo systemctl --user enable pipewire-pulse
+sudo systemctl --user enable wireplumber
+systemctl enable preload
+mkdir -P ~/.config/wal/
+mkdir ~/.config/hypr
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cd
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+cd
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+cd
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+cd
 git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
-sudo pacman -S tmux
 cd
-git clone --single-branch https://github.com/gpakosz/.tmux.git
-ln -s -f .tmux/.tmux.conf
-cp .tmux/.tmux.conf.local .
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
 cd
-paru -S lazy-nvim
-cd
-sudo pacman -S fastfetch
-mv ~/Sleepydots/fastfetch ~/.config/
-mv ~/Sleepydots/kitty ~/.config/
-mv ~/Sleepydots/wal ~/.config/
-mv ~/Sleepydots/zsh ~/
-mv ~/Sleepydots/hyprland.conf ~/.config/hypr/
-mv ~/Sleepydots/init.lua ~/.config/nvim
-echo "Done :)"
+mv ~/Sleepydotts-Hyprland-dotfiles/fastfetch/ ~/.config 
+print "Fastfetch done"
+mv ~/Sleepydots-Hyprland-dotfiles/hyprland.conf ~/.config/hypr
+print "Hyprland done"
+mv Sleepydots-Hyprland-dotfiles/wal/templates/ ~/.config/wal/
+print "Pywall done"
+cat Sleepydots-Hyprland-dotfiles/zsh/.zshrc > ~/.zshrc
+print "Zsh done"
+mv Sleepydots-Hyprland-dotfiles/kitty ~/.config
+print "You should be done now please check for any errors above ^"
+print "If you found nothing then you can now enjoy my dotfiles :)"
+print "Goodnight Zzz"
